b61b7b46768b121b221469429267d42e18292da4
Nicoratty <Nicoratty1@gmail.com>
2023-07-19 07:36:08 -0700

    chnage

--- Server/index.js
+++ Server/index.js
@@ -14,9 +14,13 @@ const profileRoute = require("./routes/profile");
 
 const bodyParser = require("body-parser");
 
+// Load environment variables from .env file
+dotenv.config();
 
-const allowedOrigins = ["https://chatter-seven-rust.vercel.app/", "https://chatterapp-gilt.vercel.app/", "http://localhost:3000"]; // Add more origins if needed.
+// Allowed origins for CORS
+const allowedOrigins = ["https://chatter-seven-rust.vercel.app/", "https://chatterapp-gilt.vercel.app/", "http://localhost:3000"];
 
+// CORS options with custom origin check
 const corsOptions = {
   origin: function (origin, callback) {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
@@ -27,16 +31,17 @@ const corsOptions = {
   },
 };
 
+// Middleware
+app.use(cors(corsOptions)); // Enable CORS with custom options
+app.use(helmet()); // Helmet for security headers
+app.use(morgan("common")); // Morgan for request logging
+app.use(express.json()); // Parse JSON requests
+app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded requests
 
-app.use(cors(corsOptions));
+// Serve static images from the "public/images" directory
 app.use("/images", express.static(path.join(__dirname, "public/images")));
-app.use(cors());
-app.use(express.json());
-app.use(helmet());
-app.use(morgan("common"));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
+// Multer storage for file uploads
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, path.join(__dirname, "/public/images"));
@@ -47,21 +52,35 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+// File upload route
 app.post("/api/upload", upload.single("file"), (req, res) => {
   try {
     return res.status(200).json("File uploaded");
   } catch (err) {
     console.log(err);
+    // You might want to send an error response here if the upload fails
+    res.status(500).json("File upload failed");
   }
 });
-app.get('/', (req,res) => {
-  res.send('welcome')
-})
+
+// Welcome route
+app.get("/", (req, res) => {
+  res.send("Welcome to the Chatter API");
+});
+
+// Routes
 app.use("/api/profile", profileRoute);
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
-dotenv.config();
-mongoose.connect(process.env.MONGO_URL);
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Start the server
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
