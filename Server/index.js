bf5f97226e9e4d866cbb0fd7ce4d7afb51042190
Nicoratty <Nicoratty1@gmail.com>
2023-07-24 04:31:50 -0700

    change

--- Server/index.js
+++ Server/index.js
@@ -81,7 +81,7 @@ app.put('/api/users/:id', upload.single('profilePicture'), async (req, res) => {
  
   if (req.file) {
     // Handle profile picture update
-    const profilePicturePath = req.file.path;
+    const profilePicturePath = req.file;
 console.log(profilePicturePath)
     try {
       // Update the user's profile picture in the database
